4fe0a4c0a714fcdfe2da1030e122c3810a618346
loisRK <loiskim150@gmail.com>
2022-12-15 17:39:03 +0900

    s3 spring 연결 추가 - create, update 수정 완료 - delete 수정 필요

---
+++ src/main/java/com/spring/gugu/config/AwsConfig.java
@@ -0,0 +1,34 @@
+package com.spring.gugu.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AwsConfig {
+	
+	@Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+	
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+    	// aws access, secret key 객체 생성
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey,secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                    .withRegion(region)
+                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                    .build();
+    }
+
+}
